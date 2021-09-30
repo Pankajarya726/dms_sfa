69ad0b543c83787b000b3b238f58e9505f491258
mac <sharma.rachna0312@gmail.com>
2021-09-30 17:06:49 +0530

    30 Sept 2021

--- lib/splash_screen/splash_screen.dart
+++ lib/splash_screen/splash_screen.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:sfa/login_screen/login_screen.dart';
 import 'package:sfa/utility/colors.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -11,9 +13,46 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
+    Timer(
+        const Duration(seconds: 3),
+        () => Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (BuildContext context) => const LoginScreen())));
     return Scaffold(
       backgroundColor: colorPrimary,
-      body: Container(),
+      body: Column(
+        children: <Widget>[
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.75,
+            width: MediaQuery.of(context).size.width,
+            child: Stack(
+              children: <Widget>[
+                Image.asset("assets/splash-triangle.png"),
+                Container(
+                  margin: const EdgeInsets.fromLTRB(40, 200, 0, 0),
+                  height: 100,
+                  width: 180,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                    image: AssetImage("assets/logo.png"),
+                  )),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.fromLTRB(0, 120, 0, 0),
+            alignment: Alignment.center,
+            height: 45,
+            width: 240,
+            decoration: BoxDecoration(
+                color: colorTabBG, borderRadius: BorderRadius.circular(50)),
+            child: const Text(
+              "Sales Force Automation",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/utility/colors.dart
+++ lib/utility/colors.dart
@@ -1,3 +1,11 @@
 import 'package:flutter/material.dart';
 
 const colorPrimary = Color(0xfff24b55);
+const colorGray = Color(0xff717171);
+const colorGrayLite = Color(0xfff2f2f2);
+const colorGrayDark = Color(0xff555555);
+const colorYellow = Color(0xfff6ac56);
+const colorRed = Color(0xfff0111f);
+const colorGreen = Color(0xff2cb743);
+const colorTabBG = Color(0xfffbcacd);
+const colorCalenderDateBG = Color(0xfff8a5aa);
