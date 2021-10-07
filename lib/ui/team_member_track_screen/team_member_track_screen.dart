e6369b9b9d0d5be866a845e3faf2c250dfda6a0c
himanshu.sharma <himanshu.sharma@tekzee.com>
2021-10-07 19:05:11 +0530

    7 Oct 2021

--- lib/ui/team_member_track_screen/team_member_track_screen.dart
+++ lib/ui/team_member_track_screen/team_member_track_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-// import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class TeamMemberTrackScreen extends StatefulWidget {
   const TeamMemberTrackScreen({Key? key}) : super(key: key);
@@ -12,22 +12,26 @@ class TeamMemberTrackScreen extends StatefulWidget {
 class _TeamMemberTrackScreenState extends State<TeamMemberTrackScreen> {
   List<Marker> markers = [];
   late GoogleMapController googleMapController;
-  late LatLng currenPosition;
-
+  LatLng? currenPosition;
+  Location location = Location();
   @override
   void initState() {
-    // getUserLocation();
+    getUserLocation();
     super.initState();
   }
 
-  // getUserLocation() async {
-  //   Position position = await Geolocator.getCurrentPosition(
-  //       desiredAccuracy: LocationAccuracy.high);
-  //   setState(() {
-  //     currenPosition = LatLng(position.latitude, position.longitude);
-  //   });
-  //   debugPrint("Current Position" + currenPosition.toString());
-  // }
+  getUserLocation() async {
+    await location.requestPermission();
+    PermissionStatus status = await location.hasPermission();
+
+    if (status == PermissionStatus.granted) {
+      LocationData position = await location.getLocation();
+      setState(() {
+        currenPosition = LatLng(position.latitude!, position.longitude!);
+      });
+      debugPrint("Current Position" + currenPosition.toString());
+    }
+  }
 
   addMarker(currenPosition) {
     setState(
@@ -56,21 +60,23 @@ class _TeamMemberTrackScreenState extends State<TeamMemberTrackScreen> {
             topRight: Radius.circular(20),
           ),
         ),
-        child: GoogleMap(
-          mapType: MapType.normal,
-          compassEnabled: true,
-          zoomControlsEnabled: true,
-          scrollGesturesEnabled: true,
-          markers: Set.from(markers),
-          initialCameraPosition:
-              CameraPosition(target: currenPosition, zoom: 14.0),
-          onMapCreated: (controller) {
-            setState(() {
-              googleMapController = controller;
-              addMarker(currenPosition);
-            });
-          },
-        ),
+        child: currenPosition != null
+            ? GoogleMap(
+                mapType: MapType.normal,
+                compassEnabled: true,
+                zoomControlsEnabled: true,
+                scrollGesturesEnabled: true,
+                markers: Set.from(markers),
+                initialCameraPosition:
+                    CameraPosition(target: currenPosition!, zoom: 14.0),
+                onMapCreated: (controller) {
+                  setState(() {
+                    googleMapController = controller;
+                    addMarker(currenPosition);
+                  });
+                },
+              )
+            : Container(),
       ),
     );
   }
