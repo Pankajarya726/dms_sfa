222ffe7ba109938d41629253f4862152b415f57f
rishabh.panchal <rishabh.panchal@tekzee.com>
2021-12-03 19:01:55 +0530

    dec 3

--- lib/ui/drawer_menu/home_screen/home_screen.dart
+++ lib/ui/drawer_menu/home_screen/home_screen.dart
@@ -10,14 +10,59 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBody: true,
-      appBar: AppBar(
-        leading: const Icon(
-          Icons.ac_unit,
-        ),
-        flexibleSpace: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Flexible(
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.menu),
+              ),
+            ),
+            const SizedBox(
+              width: 18,
+            ),
+            const Flexible(
+              child: Icon(Icons.alarm, size: 50),
+            ),
+            const SizedBox(
+              width: 18,
+            ),
+            Flexible(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "employeeName",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white30,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Text(
+                        "subtitle",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
