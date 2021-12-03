619a5c2685e6a1aed4f45aeb4f2c69726c19f6d0
rishabh.panchal <rishabh.panchal@tekzee.com>
2021-12-03 17:37:13 +0530

    dec 3

---
+++ lib/ui/drawer_menu/home_screen/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:kf_drawer/kf_drawer.dart';
+
+class HomeScreen extends KFDrawerContent {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true,
+      backgroundColor: Colors.white,
+    );
+  }
+}
