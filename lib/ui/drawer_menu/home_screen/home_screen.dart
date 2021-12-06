3e63d2576b9c95f05f5e2d0d5022c37f58736e0d
rishabh.panchal <rishabh.panchal@tekzee.com>
2021-12-06 18:44:39 +0530

    dec 6

--- lib/ui/drawer_menu/home_screen/home_screen.dart
+++ lib/ui/drawer_menu/home_screen/home_screen.dart
@@ -12,31 +12,66 @@ class HomeScreen extends KFDrawerContent {
 class _HomeScreenState extends State<HomeScreen> {
   int currentBottomTabIndex = 0;
 
+  List<String> itemLabels = [
+    "Team Perform",
+    "Market Visit",
+    "Task",
+    "Enrollment",
+    "Order Booking",
+    "Dealer Info",
+  ];
+
+  List<String> itemIcons = [
+    "assets/team_perform.png",
+    "assets/market_visit.png",
+    "assets/task.png",
+    "assets/enrollment.png",
+    "assets/order_booking.png",
+    "assets/dealer_info.png",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.white,
+        backgroundColor: const Color(0xFFFAFAFA),
         appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(67.0),
+          preferredSize: const Size.fromHeight(75.0),
           child: AppBar(
-            elevation: 3,
+            elevation: 5,
+            shadowColor: Colors.black26,
             backgroundColor: Colors.white,
-            titleSpacing: 8,
-            leading: Align(
-              child: InkWell(
-                onTap: widget.onMenuPressed,
-                child: SvgPicture.asset(
-                  "assets/menu.svg",
-                  height: 30,
-                  width: 30,
-                  fit: BoxFit.contain,
-                ),
-              ),
-            ),
-            title: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            flexibleSpace: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
+                const SizedBox(
+                  width: 5,
+                ),
+                Material(
+                  color: Colors.transparent,
+                  borderRadius: BorderRadius.circular(30),
+                  child: InkWell(
+                    customBorder: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    onTap: widget.onMenuPressed,
+                    child: SizedBox(
+                      width: 60,
+                      height: 60,
+                      child: Align(
+                        child: SvgPicture.asset(
+                          "assets/menu.svg",
+                          height: 30,
+                          width: 30,
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 15,
+                ),
                 ClipRRect(
                   borderRadius: BorderRadius.circular(50),
                   child: CachedNetworkImage(
@@ -56,14 +91,16 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
                 Flexible(
                   child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       const Text(
-                        "Employee Name",
+                        "Smith Johnson",
                         style: TextStyle(
                           color: Colors.black,
                           fontSize: 20,
                           fontWeight: FontWeight.bold,
+                          overflow: TextOverflow.ellipsis,
                         ),
                       ),
                       Container(
@@ -71,8 +108,8 @@ class _HomeScreenState extends State<HomeScreen> {
                         padding: const EdgeInsets.symmetric(
                             vertical: 4, horizontal: 6),
                         decoration: BoxDecoration(
-                          color: Colors.pink[100],
-                          borderRadius: BorderRadius.circular(5),
+                          color: const Color(0xFFF3505A).withOpacity(0.3),
+                          borderRadius: BorderRadius.circular(3),
                         ),
                         child: const Text(
                           "Employee Designation",
@@ -80,76 +117,148 @@ class _HomeScreenState extends State<HomeScreen> {
                             fontWeight: FontWeight.w500,
                             color: Colors.black,
                             fontSize: 15,
+                            overflow: TextOverflow.ellipsis,
                           ),
                         ),
                       ),
                     ],
                   ),
+                ),
+                const SizedBox(
+                  width: 15,
                 )
               ],
             ),
           ),
         ),
-        bottomNavigationBar: BottomNavigationBar(
-          selectedFontSize: 12,
-          unselectedFontSize: 12,
-          selectedItemColor: Colors.red,
-          showUnselectedLabels: true,
-          unselectedItemColor: Colors.grey[600],
-          currentIndex: currentBottomTabIndex,
-          type: BottomNavigationBarType.fixed,
-          onTap: ontemTaped,
-          elevation: 15,
-          items: [
-            BottomNavigationBarItem(
-              icon: Container(
-                padding: const EdgeInsets.symmetric(vertical: 4),
-                child: SvgPicture.asset(
-                  "assets/menu.svg",
-                  height: 25,
-                  width: 25,
-                  fit: BoxFit.contain,
+        bottomNavigationBar: Container(
+          decoration: const BoxDecoration(
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 10,
+              ),
+            ],
+          ),
+          child: BottomNavigationBar(
+            selectedFontSize: 12,
+            unselectedFontSize: 12,
+            selectedItemColor: Colors.grey[600],
+            currentIndex: currentBottomTabIndex,
+            type: BottomNavigationBarType.fixed,
+            onTap: ontemTaped,
+            elevation: 5,
+            unselectedLabelStyle: const TextStyle(
+              fontWeight: FontWeight.w500,
+            ),
+            selectedLabelStyle: const TextStyle(
+              fontWeight: FontWeight.w500,
+            ),
+            items: [
+              BottomNavigationBarItem(
+                icon: Container(
+                  padding: const EdgeInsets.symmetric(vertical: 1),
+                  child: SvgPicture.asset(
+                    "assets/tutorials.svg",
+                    height: 25,
+                    width: 25,
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                label: "Tutorials",
+              ),
+              BottomNavigationBarItem(
+                icon: Container(
+                  padding: const EdgeInsets.symmetric(vertical: 1),
+                  child: SvgPicture.asset(
+                    "assets/product.svg",
+                    height: 25,
+                    width: 25,
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                label: "Product",
+              ),
+              BottomNavigationBarItem(
+                icon: Container(
+                  padding: const EdgeInsets.symmetric(vertical: 1),
+                  child: SvgPicture.asset(
+                    "assets/performance.svg",
+                    height: 25,
+                    width: 25,
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                label: "Performance",
+              ),
+              BottomNavigationBarItem(
+                icon: Container(
+                  padding: const EdgeInsets.symmetric(vertical: 1),
+                  child: SvgPicture.asset(
+                    "assets/plan.svg",
+                    height: 25,
+                    width: 25,
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                label: "Plan",
+              ),
+            ],
+          ),
+        ),
+        body: GridView.count(
+          crossAxisCount: 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+          childAspectRatio: 0.9,
+          padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 15),
+          children: List.generate(6, (index) {
+            return Container(
+              height: 400,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: const [
+                  BoxShadow(
+                    offset: Offset(0, 0),
+                    blurRadius: 10,
+                    color: Color.fromRGBO(0, 0, 0, 0.10),
+                  )
+                ],
+              ),
+              child: Material(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                child: InkWell(
+                  customBorder: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  onTap: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 30),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          itemIcons[index],
+                          fit: BoxFit.contain,
+                        ),
+                        const SizedBox(height: 25),
+                        Text(
+                          itemLabels[index],
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17,
+                            letterSpacing: 0.67,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ),
-              label: "Tutorials",
-            ),
-            BottomNavigationBarItem(
-              icon: Container(
-                padding: const EdgeInsets.symmetric(vertical: 4),
-                child: SvgPicture.asset(
-                  "assets/menu.svg",
-                  height: 25,
-                  width: 25,
-                  fit: BoxFit.contain,
-                ),
-              ),
-              label: "Product",
-            ),
-            BottomNavigationBarItem(
-              icon: Container(
-                padding: const EdgeInsets.symmetric(vertical: 4),
-                child: SvgPicture.asset(
-                  "assets/menu.svg",
-                  height: 25,
-                  width: 25,
-                  fit: BoxFit.contain,
-                ),
-              ),
-              label: "Performance",
-            ),
-            BottomNavigationBarItem(
-              icon: Container(
-                padding: const EdgeInsets.symmetric(vertical: 4),
-                child: SvgPicture.asset(
-                  "assets/menu.svg",
-                  height: 25,
-                  width: 25,
-                  fit: BoxFit.contain,
-                ),
-              ),
-              label: "Plan",
-            ),
-          ],
+            );
+          }),
         ),
       ),
     );
