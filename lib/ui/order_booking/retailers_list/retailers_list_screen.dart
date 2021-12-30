6251880b776b3b61ad932c037ac5d61ff04ab560
Pankaj Arya <pankaj.arya@tekzee.com>
2021-12-30 17:02:20 +0530

    commit change at 30-12-2021 at 5:01 pm

---
+++ lib/ui/order_booking/retailers_list/retailers_list_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RetailerListScreen extends StatefulWidget {
+  const RetailerListScreen({Key? key}) : super(key: key);
+
+  @override
+  _RetailerListScreenState createState() => _RetailerListScreenState();
+}
+
+class _RetailerListScreenState extends State<RetailerListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
